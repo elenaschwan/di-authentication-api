f85469e9fa94d3224b3ca04a78114096b6601bfb
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2022-12-12 17:06:07 +0000

    initial spike on passkeys

---
+++ frontend-api/src/main/java/uk/gov/di/authentication/frontendapi/lambda/PasskeyCreateHandler.java
@@ -0,0 +1,112 @@
+package uk.gov.di.authentication.frontendapi.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.gov.di.authentication.frontendapi.domain.FrontendAuditableEvent;
+import uk.gov.di.authentication.frontendapi.entity.MfaRequest;
+import uk.gov.di.authentication.shared.domain.AuditableEvent;
+import uk.gov.di.authentication.shared.entity.ClientRegistry;
+import uk.gov.di.authentication.shared.entity.ErrorResponse;
+import uk.gov.di.authentication.shared.entity.NotificationType;
+import uk.gov.di.authentication.shared.entity.NotifyRequest;
+import uk.gov.di.authentication.shared.entity.Session;
+import uk.gov.di.authentication.shared.exceptions.ClientNotFoundException;
+import uk.gov.di.authentication.shared.helpers.IpAddressHelper;
+import uk.gov.di.authentication.shared.helpers.PersistentIdHelper;
+import uk.gov.di.authentication.shared.lambda.BaseFrontendHandler;
+import uk.gov.di.authentication.shared.serialization.Json.JsonException;
+import uk.gov.di.authentication.shared.services.AuditService;
+import uk.gov.di.authentication.shared.services.AuthenticationService;
+import uk.gov.di.authentication.shared.services.AwsSqsClient;
+import uk.gov.di.authentication.shared.services.ClientService;
+import uk.gov.di.authentication.shared.services.ClientSessionService;
+import uk.gov.di.authentication.shared.services.CodeGeneratorService;
+import uk.gov.di.authentication.shared.services.CodeStorageService;
+import uk.gov.di.authentication.shared.services.ConfigurationService;
+import uk.gov.di.authentication.shared.services.RedisConnectionService;
+import uk.gov.di.authentication.shared.services.SessionService;
+import uk.gov.di.authentication.shared.state.UserContext;
+
+import java.util.Locale;
+import java.util.Optional;
+
+import static uk.gov.di.authentication.shared.entity.ErrorResponse.ERROR_1000;
+import static uk.gov.di.authentication.shared.entity.ErrorResponse.ERROR_1001;
+import static uk.gov.di.authentication.shared.entity.ErrorResponse.ERROR_1014;
+import static uk.gov.di.authentication.shared.entity.NotificationType.MFA_SMS;
+import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateApiGatewayProxyErrorResponse;
+import static uk.gov.di.authentication.shared.helpers.ApiGatewayResponseHelper.generateEmptySuccessApiGatewayResponse;
+import static uk.gov.di.authentication.shared.helpers.LogLineHelper.LogFieldName.CLIENT_ID;
+import static uk.gov.di.authentication.shared.helpers.LogLineHelper.LogFieldName.PERSISTENT_SESSION_ID;
+import static uk.gov.di.authentication.shared.helpers.LogLineHelper.attachLogFieldToLogs;
+import static uk.gov.di.authentication.shared.helpers.LogLineHelper.attachSessionIdToLogs;
+import static uk.gov.di.authentication.shared.services.CodeStorageService.CODE_BLOCKED_KEY_PREFIX;
+import static uk.gov.di.authentication.shared.services.CodeStorageService.CODE_REQUEST_BLOCKED_KEY_PREFIX;
+
+public class PasskeyCreateHandler extends BaseFrontendHandler<MfaRequest>
+        implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private static final Logger LOG = LogManager.getLogger(MfaHandler.class);
+
+    private final CodeGeneratorService codeGeneratorService;
+    private final CodeStorageService codeStorageService;
+    private final AuditService auditService;
+    private final AwsSqsClient sqsClient;
+
+    public MfaHandler(
+            ConfigurationService configurationService,
+            SessionService sessionService,
+            CodeGeneratorService codeGeneratorService,
+            CodeStorageService codeStorageService,
+            ClientSessionService clientSessionService,
+            ClientService clientService,
+            AuthenticationService authenticationService,
+            AuditService auditService,
+            AwsSqsClient sqsClient) {
+        super(
+                MfaRequest.class,
+                configurationService,
+                sessionService,
+                clientSessionService,
+                clientService,
+                authenticationService);
+        this.codeGeneratorService = codeGeneratorService;
+        this.codeStorageService = codeStorageService;
+        this.auditService = auditService;
+        this.sqsClient = sqsClient;
+    }
+
+    public MfaHandler() {
+        super(MfaRequest.class, ConfigurationService.getInstance());
+        this.codeGeneratorService = new CodeGeneratorService();
+        this.codeStorageService =
+                new CodeStorageService(new RedisConnectionService(configurationService));
+        this.auditService = new AuditService(configurationService);
+        this.sqsClient =
+                new AwsSqsClient(
+                        configurationService.getAwsRegion(),
+                        configurationService.getEmailQueueUri(),
+                        configurationService.getSqsEndpointUri());
+    }
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequestWithUserContext(
+            APIGatewayProxyRequestEvent input,
+            Context context,
+            MfaRequest request,
+            UserContext userContext) {
+        try {
+            
+        } catch (JsonException e) {
+            return generateApiGatewayProxyErrorResponse(400, ERROR_1001);
+        } catch (ClientNotFoundException e) {
+            LOG.warn("Client not found");
+            return generateApiGatewayProxyErrorResponse(400, ErrorResponse.ERROR_1015);
+        }
+    }
+
+}
